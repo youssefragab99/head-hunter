e10285530b7803af24bfa39a18050ee7adb1c26a
Youssef Ragab <yragab99@icloud.com>
2023-12-17 02:19:42 -0500

    Created client object

--- src/open_ai_helper.py
+++ src/open_ai_helper.py
@@ -8,38 +8,22 @@ with open("keys.yaml", "r") as f:
 os.environ["OPENAI_API_KEY"] = ""
 
 
-class OpenAiHelper:
+class Client:
     def __init__(self, api_key):
         self.api_key = api_key
-        self.open_ai = OpenAI()
-
-    def get_answer(self, question, context):
-        # response = self.open_ai.complete(
-        #     engine="turbo 3.5",
-        #     prompt=f"Q: {question}\nA:",
-        #     max_tokens=100,
-        #     temperature=0.3,
-        #     top_p=1,
-        #     frequency_penalty=0,
-        #     presence_penalty=0,
-        #     stop=["\n"],
-        # )
-        response = self.open_ai.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": f"Q: {question}\nA:",
-                }
-            ],
-            model="gpt-3.5-turbo",
-        )
-        answer = response.choices[0].text.strip()
-        return answer
+        self.client = OpenAI(api_key=api_key)
 
 
-if __name__ == "__main__":
-    open_ai_helper = OpenAiHelper(api_key=open_ai_key)
-    question = "What is the capital of Italy?"
-    context = "Rome is the capital of Italy."
-    answer = open_ai_helper.get_answer(question, context)
-    print(answer)
+class Assistant:
+    def __init__(self, client):
+        self.client = client 
+        
+    def retrieve_client(self)
+
+
+# if __name__ == "__main__":
+#     open_ai_helper = OpenAiHelper(api_key=open_ai_key)
+#     question = "What is the capital of Italy?"
+#     context = "Rome is the capital of Italy."
+#     answer = open_ai_helper.get_answer(question, context)
+#     print(answer)
